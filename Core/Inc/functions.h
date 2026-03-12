46c40791514f54d111f46c002c9734bd28c88706
tangwp1588 <tangweipeng6286@gmail.com>
2026-03-12 16:07:14 +0800

    Delete Core directory

--- Core/Inc/functions.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int map(int x, int in_min, int in_max, int out_min, int out_max);
-double constrain(double value, double min, double max);
--- Core/Inc/key.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include"main.h"
-#include "cmsis_os2.h"
-uint8_t Key_GetNum(void);
